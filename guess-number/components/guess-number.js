5ac48f392f81288cd1bd68534aca3af12556f0a7
SwitchVarial <kimmo.lindeman@gmail.com>
2022-08-30 13:22:46 +0300

    guess the number

---
+++ guess-number/components/guess-number.js
@@ -0,0 +1,58 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View, Keyboard } from 'react-native';
+
+let count = 0;
+
+export default function GuessNumber() {
+    const [number] = useState(Math.round((Math.random() * 100) + 1));
+    const [message, setMessage] = useState('Guess a number between 1-100');
+    const [guess, setGuess] = useState('');
+
+    const guessPressed = () => {
+        count++;
+        Keyboard.dismiss();
+
+        if (parseInt(guess) === number) {
+            setMessage('Correct! You guessed the number in ' + count + ' guesses');
+        }
+        if (parseInt(guess) < number) {
+            setMessage('Your guess ' + guess + ' is too low');
+        }
+        if (parseInt(guess) > number) {
+            setMessage('Your guess ' + guess + ' is too high');
+        }
+    };
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>{message}</Text>
+            <TextInput style={styles.input} onChangeText={guess => setGuess(guess)} value={guess} keyboardType="number-pad"/>
+            <Button onPress={guessPressed} style={styles.button} title="Guess the Number"></Button>
+            <StatusBar style="auto" />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    text: {
+      marginBottom: 5
+    },
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    input: {
+      width: 250,
+      height: 40,
+      padding: 10,
+      margin: 5,
+      borderColor: 'gray', 
+      borderWidth: 1
+    },
+    button: {
+      padding: 5,
+      margin: 50,
+    }
+  });
